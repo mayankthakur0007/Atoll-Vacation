654bb233ab7e3eb145b3ad48024c43cd567dc720
mayankthakur0007 <bumblewellp@gmail.com>
2024-06-16 23:34:54 +0530

    modal

--- src/components/modalWrapper.jsx
+++ src/components/modalWrapper.jsx
@@ -7,14 +7,20 @@ import toast, { Toaster } from 'react-hot-toast';
 
 const ModalWrapper = (props) => {
   const [formOpen, setOpenForm] = useState(false)
+  const [packageSelected,setPackageSelected] = useState({})
   return (
-    <div>
-    <div><Toaster/></div>
+    <div >
+      <div>
+        <Toaster />
+      </div>
       <Modal
         open={props.open}
         onClose={() => {
           props.onClose();
           setOpenForm(false);
+        }}
+        classNames={{
+          modal: formOpen?"w-96":"w-max"
         }}
         center
       >
@@ -40,9 +46,10 @@ const ModalWrapper = (props) => {
                       ₹{ele.price} per person
                     </p>
                     <button
-                      onClick={() =>{
-                       setOpenForm(true) ;
-                      } }
+                      onClick={() => {
+                        setPackageSelected(ele);
+                        setOpenForm(true);
+                      }}
                       className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     >
                       Get Quotation
@@ -69,7 +76,7 @@ const ModalWrapper = (props) => {
         )}
         {formOpen && (
           <form
-            class="max-w-md font-['Caveat'] mx-auto"
+            class="font-['Caveat']"
             onSubmit={(e) => {
               e.preventDefault();
               emailjs
@@ -81,7 +88,10 @@ const ModalWrapper = (props) => {
                 )
                 .then(
                   (result) => {
-                    toast.success("Query submitted successfully. We'll get back to you shortly");
+                    toast.success(
+                      "Query submitted successfully. We'll get back to you shortly"
+                    );
+                    setPackageSelected({});
                     props.onClose();
                     //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior)
                   },
@@ -124,25 +134,35 @@ const ModalWrapper = (props) => {
                 Phone No.
               </label>
             </div>
-            <div class="grid md:grid-cols-2 md:gap-6">
-              <div class="relative z-0 w-full mb-5 group">
-                <input
-                  type="text"
-                  name="from_name"
-                  id="from_name"
-                  class="block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                  placeholder=" "
-                  required
-                />
-                <label
-                  for="from_name"
-                  class="peer-focus:font-medium absolute text-xl text-black duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >
-                  Name
-                </label>
-              </div>
+            <div class="relative z-0 w-full mb-5 group">
+              <input
+                type="text"
+                name="from_name"
+                id="from_name"
+                class="block py-2.5 px-0 w-full text-xl text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
+                required
+              />
+              <label
+                for="from_name"
+                class="peer-focus:font-medium absolute text-xl text-black duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Name
+              </label>
             </div>
-
+            <div class="relative z-0 w-full mb-5 group">
+              <input
+                type="text"
+                name="message"
+                readOnly
+                value={`${props.selectedLocation} ${packageSelected.plan} for ₹${packageSelected.price} per person`}
+                id="message"
+                class="block py-2.5 px-0 w-full text-xl text-black bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
+                required
+              />
+            </div>
+            <br></br>
             <button
               type="submit"
               class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
